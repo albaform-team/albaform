c5b2a0a2905958a33ce7189666e9f5b473ef4606
spde3289 <spde3289@naver.com>
2025-12-22 23:23:31 +0900

    feat: 스타일 린트 설정

---
+++ stylelint.config.cjs
@@ -0,0 +1,86 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-order'],
+
+  overrides: [
+    {
+      files: ['**/*.{ts,tsx}'],
+      customSyntax: 'postcss-styled-syntax',
+    },
+  ],
+
+  rules: {
+    'declaration-block-no-duplicate-properties': [
+      true,
+      { ignore: ['consecutive-duplicates-with-different-values'] },
+    ],
+    'color-no-invalid-hex': true,
+    'function-no-unknown': true,
+    'unit-no-unknown': true,
+    'property-no-unknown': true,
+    'length-zero-no-unit': true,
+
+    'order/properties-order': [
+      [
+        {
+          groupName: 'layout',
+          properties: [
+            'position',
+            'top',
+            'right',
+            'bottom',
+            'left',
+            'z-index',
+            'display',
+            'flex',
+            'flex-direction',
+            'flex-wrap',
+            'gap',
+            'align-items',
+            'justify-content',
+          ],
+        },
+        {
+          groupName: 'box-model',
+          properties: ['box-sizing', 'width', 'height', 'margin', 'padding'],
+        },
+        {
+          groupName: 'border-background',
+          properties: [
+            'border',
+            'border-radius',
+            'background',
+            'background-color',
+          ],
+        },
+        {
+          groupName: 'visual',
+          properties: ['box-shadow', 'opacity'],
+        },
+        {
+          groupName: 'typography',
+          properties: [
+            'color',
+            'font-size',
+            'font-weight',
+            'line-height',
+            'text-align',
+          ],
+        },
+        {
+          groupName: 'interaction-animation',
+          properties: ['cursor', 'transition', 'animation'],
+        },
+      ],
+      {
+        unspecified: 'bottom',
+        emptyLineBeforeUnspecified: 'always',
+      },
+    ],
+
+    // CSS-in-JS 오탐 제거
+    'no-descending-specificity': null,
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+  },
+};

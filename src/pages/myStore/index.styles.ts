e0aba7f16314342c06d5632ba38fedad337195ce
summerlane <punkwagon@gmail.com>
2025-12-24 14:26:03 +0900

    style: 스타일 린트 수정

--- src/pages/myStore/index.styles.ts
+++ src/pages/myStore/index.styles.ts
@@ -2,41 +2,41 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   flex-direction: column;
   gap: 8px;
+  align-items: center;
+  justify-content: center;
   padding: 40px 12px;
 `;
 
 export const Button = styled.button`
-  background-color: #ea3c12;
-  color: #ffffff;
-  font-size: 16px;
-  font-weight: 700;
   padding: 10px 20px;
   border-radius: 6px;
+  background-color: #ea3c12;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
 `;
 
 export const Title = styled.h1`
-  text-align: left;
   width: 100%;
   font-size: 20px;
   font-weight: 700;
+  text-align: left;
 `;
 
 export const Card = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   gap: 16px;
-  border-radius: 12px;
-  border: 1px solid #e5e4e7;
-  padding: 60px 24px;
+  align-items: center;
   width: 100%;
+  padding: 60px 24px;
+  border: 1px solid #e5e4e7;
+  border-radius: 12px;
 `;
 
 export const CardText = styled.p`
-  font-size: 14px;
   color: #111322;
+  font-size: 14px;
 `;

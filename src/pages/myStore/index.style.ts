3831e7203786a73c1760115770841fb9d98f5999
summerlane <punkwagon@gmail.com>
2025-12-26 13:14:07 +0900

    chore: 반응형 작업 추가

--- src/pages/myStore/index.style.ts
+++ src/pages/myStore/index.style.ts
@@ -1,21 +1,39 @@
 import styled from '@emotion/styled';
+import { media } from '@/styles/media';
+import { colors } from '@/styles/colors';
 
 export const Container = styled.div`
+  min-width: 375px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   gap: 8px;
   align-items: center;
   justify-content: center;
   padding: 40px 12px;
+
+  @media ${media.tablet} {
+    padding: 60px 32px;
+  }
+
+  @media ${media.desktop} {
+    max-width: 965px;
+    padding: 80px 0;
+  }
 `;
 
 export const Button = styled.button`
   padding: 10px 20px;
   border-radius: 6px;
-  background-color: #ea3c12;
-  color: #fff;
-  font-size: 16px;
+  background-color: ${colors.red[40]};
+  color: ${colors.white};
+  font-size: 14px;
   font-weight: 700;
+
+  @media ${media.tablet} {
+    min-width: 346px;
+    font-size: 16px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -23,6 +41,10 @@ export const Title = styled.h1`
   font-size: 20px;
   font-weight: 700;
   text-align: left;
+
+  @media ${media.tablet} {
+    font-size: 28px;
+  }
 `;
 
 export const Card = styled.div`
@@ -32,11 +54,15 @@ export const Card = styled.div`
   align-items: center;
   width: 100%;
   padding: 60px 24px;
-  border: 1px solid #e5e4e7;
+  border: 1px solid ${colors.gray[20]};
   border-radius: 12px;
 `;
 
 export const CardText = styled.p`
-  color: #111322;
+  color: ${colors.black};
   font-size: 14px;
+
+  @media ${media.tablet} {
+    font-size: 16px;
+  }
 `;

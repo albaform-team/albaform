55118bbb168c51c67a78572e167e4de09d573129
summerlane <punkwagon@gmail.com>
2025-12-29 20:22:02 +0900

    chore: 스타일린트 수정

--- src/pages/myStore/list/index.style.ts
+++ src/pages/myStore/list/index.style.ts
@@ -320,17 +320,17 @@ export const CardListImgWrap = styled.div`
   border-radius: 12px;
 
   > div {
+    position: relative;
     z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.7);
-    color: ${colors.gray[30]};
-    position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     font-size: 20px;
     font-weight: 700;
+    color: ${colors.gray[30]};
+    background-color: rgb(0 0 0 / 70%);
 
     @media ${media.tablet} {
       font-size: 28px;

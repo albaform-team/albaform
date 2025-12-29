6fdcc2835c9780cdf6d6c5a94da2fefc6ebb23f2
summerlane <punkwagon@gmail.com>
2025-12-29 20:21:43 +0900

    chore: 마감 완료 불투명창 css 스타일링

--- src/pages/myStore/list/index.style.ts
+++ src/pages/myStore/list/index.style.ts
@@ -319,6 +319,24 @@ export const CardListImgWrap = styled.div`
   background-color: ${colors.gray[50]};
   border-radius: 12px;
 
+  > div {
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: ${colors.gray[30]};
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    font-weight: 700;
+
+    @media ${media.tablet} {
+      font-size: 28px;
+    }
+  }
+
   @media ${media.tablet} {
     height: 172px;
   }
@@ -474,7 +492,7 @@ export const CardPriceSubText = styled.p`
 export const CardPriceSubTextDisable = styled.p`
   font-size: 12px;
   font-weight: 400;
-  color: ${colors.gray[30]};
+  color: ${colors.gray[20]};
   letter-spacing: -1px;
 
   @media ${media.tablet} {
@@ -557,7 +575,7 @@ export const ArrowIcon = styled(ArrowIconComponent)`
 
 export const ArrowIconDisable = styled(ArrowIconComponent)`
   path {
-    fill: ${colors.gray[30]};
+    fill: ${colors.gray[20]};
   }
 
   @media ${media.tablet} {

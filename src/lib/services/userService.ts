c8ae43ed4caa05fcf0f7167598ec383c5bd3da6f
spde3289 <spde3289@naver.com>
2025-12-30 16:49:09 +0900

    chore: 목업 데이터 수정

--- src/lib/services/userService.ts
+++ src/lib/services/userService.ts
@@ -1,6 +1,9 @@
 import _MOCK_USER_APPLICATIONS from '@/__MOCK/_MOCK_USER_APPLICATIONS.json';
 import { APPLICATIONS_API, USERS_API } from '@/constants/api';
-import { appliedJobListMapper } from '@/pages/profile/[userId]/_utils/mapper';
+import {
+  appliedJobListMapper,
+  AppliedJobListViewModel,
+} from '@/pages/profile/[userId]/_utils/mapper';
 import { UserInfoResponse } from '@/types/api/user';
 import { AppliedJobListResponse } from '@/types/api/userAppliedJobList';
 
@@ -16,7 +19,7 @@ export const GetUserApplications = async (
   userId: string,
   offset: number = 0,
   limit: number
-) => {
+): Promise<AppliedJobListViewModel> => {
   const { data } = await services.get<AppliedJobListResponse>(
     APPLICATIONS_API.USER_LIST(userId),
     { params: { offset, limit } }
@@ -24,5 +27,15 @@ export const GetUserApplications = async (
 
   console.log(appliedJobListMapper(data).items[0]);
 
-  return _MOCK_USER_APPLICATIONS.splice(offset, limit);
+  return {
+    items: _MOCK_USER_APPLICATIONS.splice(
+      offset,
+      limit
+    ) as AppliedJobListViewModel['items'],
+    count: _MOCK_USER_APPLICATIONS.length,
+    hasNext: true,
+    offset,
+    limit,
+    links: data.links,
+  };
 };

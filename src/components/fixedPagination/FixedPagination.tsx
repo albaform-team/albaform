216448ca4ae2247af6c39970bc7222d432d5b84b
spde3289 <spde3289@naver.com>
2025-12-30 21:06:20 +0900

    feat: 고정 페이지네이션 버튼 컴포넌트

---
+++ src/components/fixedPagination/FixedPagination.tsx
@@ -0,0 +1,48 @@
+import usePagination from '@mui/material/usePagination';
+
+import { getVisiblePages } from '@/utils/pagination';
+
+import * as S from './FixedPagination.style';
+
+interface Props {
+  page: number;
+  count: number;
+  onChange: (page: number) => void;
+  visibleCount?: number;
+}
+
+const FixedPagination = ({
+  page,
+  count,
+  visibleCount = 7,
+  onChange,
+}: Props) => {
+  const { items } = usePagination({
+    page,
+    count,
+    boundaryCount: count / 2,
+    onChange: (_, p) => onChange(p),
+  });
+
+  const visiblePages = getVisiblePages(page, count, visibleCount);
+
+  return (
+    <S.FixedPaginationWrapper direction="row" spacing={1}>
+      {items
+        .filter(item => {
+          if (item.type === 'previous' || item.type === 'next') {
+            return true;
+          }
+
+          if (item.type === 'page') {
+            return visiblePages.includes(item.page!);
+          }
+        })
+        .map((item, idx) => (
+          <S.TablePaginationItem key={idx} {...item} shape="rounded" />
+        ))}
+    </S.FixedPaginationWrapper>
+  );
+};
+
+export default FixedPagination;

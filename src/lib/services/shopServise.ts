aeaa302c2664fc01b804f1d0c5ba0c6754306f36
spde3289 <spde3289@naver.com>
2026-01-01 04:01:03 +0900

    feat: 공고 지원자 목록 목업 데이터, api 요청 함수

---
+++ src/lib/services/shopServise.ts
@@ -0,0 +1,47 @@
+import { _MOCK_SHOP_NOTICES_APPLICATIONS } from '@/__MOCK/_MOCK_SHOP_NOTICES_APPLICATIONS';
+import _MOCK_SHOP_NOTICES_DEATIL from '@/__MOCK/_MOCK_SHOP_NOTICES_DEATIL.json';
+import { APPLICATIONS_API, NOTICES_API } from '@/constants/api';
+import {
+  ApplicationStatus,
+  ShopNoticeApplicationsItem,
+  ShopNoticeApplicationsResponse,
+} from '@/types/api/shopNoticeApplications';
+import { ShopNoticeDetailResponse } from '@/types/api/shopNoticeDetail';
+
+import { services } from './servicesClient';
+
+export const getShopNoticeDetail = async (shopId: string, noticeId: string) => {
+  const { data } = await services.get<ShopNoticeDetailResponse>(
+    NOTICES_API.SHOP_DETAIL(shopId, noticeId)
+  );
+  console.log(data);
+  return _MOCK_SHOP_NOTICES_DEATIL;
+};
+
+export const getShopNoticeApplications = async (
+  shopId: string,
+  noticeId: string,
+  offset: number,
+  limit: number
+) => {
+  const { data } = await services.get<ShopNoticeApplicationsResponse>(
+    APPLICATIONS_API.SHOP_NOTICE_LIST(shopId, noticeId),
+    { params: { offset, limit } }
+  );
+  console.log(data);
+  return _MOCK_SHOP_NOTICES_APPLICATIONS;
+};
+
+export const updateJobApplicationStatus = async (
+  shopId: string,
+  noticeId: string,
+  applicationId: string,
+  status: ApplicationStatus
+) => {
+  const { data } = await services.put<ShopNoticeApplicationsItem>(
+    APPLICATIONS_API.SHOP_NOTICE_DETAIL(shopId, noticeId, applicationId),
+    { status }
+  );
+  console.log(data);
+  return status;
+};

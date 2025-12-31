1e979102508c761b17ea7e37c8bf3e1504651956
spde3289 <spde3289@naver.com>
2026-01-01 04:01:42 +0900

    feat: 파일 위치 변경

---
+++ src/pages/myStore/jobs/[jobId]/index.page.style.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+export const JobDetailLayout = styled.main`
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 180px);
+  margin: 0 auto;
+`;
---
+++ src/pages/myStore/jobs/[jobId]/index.page.tsx
@@ -0,0 +1,11 @@
+import * as S from './index.page.style';
+
+const JobDetailPage = () => {
+  return (
+    <S.JobDetailLayout>
+      <div>공고 상세 페이지</div>
+    </S.JobDetailLayout>
+  );
+};
+
+export default JobDetailPage;

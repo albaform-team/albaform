bf57200e8491ab43dc1d73b9039df7d60cb7787b
summerlane <punkwagon@gmail.com>
2026-01-02 22:53:06 +0900

    feat: 공고 등록 편집 기능 작업

--- src/pages/myStore/jobs/[id].page.tsx
+++ src/pages/myStore/jobs/[id].page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 
 import Modal from '@mui/material/Modal';
+import axios from 'axios';
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 import CloseIcon from '@/assets/svg/closeicon.svg';
+import useAuthStore from '@/stores/useAuthStore';
 
 import * as S from './new.style';
 
@@ -12,6 +15,120 @@ const JobRegisterIdPage = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  // 시급
+  const [pay, setPay] = useState('');
+
+  const payInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPay(e.target.value);
+  };
+
+  // 시작 일시
+  const [time, setTime] = useState('');
+
+  const handleTime = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTime(e.target.value);
+  };
+
+  // 없무 시간
+  const [workTime, setWorkTime] = useState('');
+
+  const handleWorkTime = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setWorkTime(e.target.value);
+  };
+
+  // 공고 설명
+  const [jobExplain, setJobExplain] = useState('');
+
+  const textAreaInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setJobExplain(e.target.value);
+  };
+
+  useEffect(() => {
+    const importData = async () => {
+      const getInfo = sessionStorage.getItem('auth-storage');
+      const getUser = JSON.parse(getInfo as string);
+      const getUserId = getUser.state.user.id;
+
+      const getShop = await axios.get(
+        `https://bootcamp-api.codeit.kr/api/0-1/the-julge/users/${getUserId}`
+      );
+      const shopId = getShop.data.item.shop.item.id;
+
+      const getNotice = await axios.get(
+        `https://bootcamp-api.codeit.kr/api/0-1/the-julge/shops/${shopId}/notices`
+      );
+
+      const getPay = getNotice.data.items[0].item.hourlyPay;
+      setPay(getPay);
+
+      const toDatetimeLocal = (date: Date) => {
+        const offset = date.getTimezoneOffset() * 60000;
+        const localDate = new Date(date.getTime() - offset);
+        return localDate.toISOString().slice(0, 16);
+      };
+      const getTime = getNotice.data.items[0].item.startsAt;
+      const changeTime = toDatetimeLocal(new Date(getTime));
+      setTime(changeTime);
+
+      const getWorkTime = getNotice.data.items[0].item.workhour;
+      setWorkTime(getWorkTime);
+
+      const getJobExplain = getNotice.data.items[0].item.description;
+      setJobExplain(getJobExplain);
+    };
+
+    importData();
+  }, []);
+
+  // submit 관련
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { accessToken } = useAuthStore.getState();
+
+    const getInfo = sessionStorage.getItem('auth-storage');
+    const getUser = JSON.parse(getInfo as string);
+    const getUserId = getUser.state.user.id;
+
+    const getShop = await axios.get(
+      `https://bootcamp-api.codeit.kr/api/0-1/the-julge/users/${getUserId}`
+    );
+    const shopId = getShop.data.item.shop.item.id;
+
+    const getNotice = await axios.get(
+      `https://bootcamp-api.codeit.kr/api/0-1/the-julge/shops/${shopId}/notices`
+    );
+
+    const noticeId = getNotice.data.items[0].item.id;
+
+    try {
+      await axios.put(
+        `https://bootcamp-api.codeit.kr/api/0-1/the-julge/shops/${shopId}/notices/${noticeId}`,
+        {
+          hourlyPay: Number(pay),
+          startsAt: new Date(time).toISOString(),
+          workhour: Number(workTime),
+          description: jobExplain,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (!shopId) {
+        console.log('shopId가 없습니다');
+        return;
+      }
+
+      handleOpen();
+      console.log('수정이 완료되었습니다.');
+    } catch (error) {
+      console.log('수정에 실패하였습니다.', error);
+    }
+  };
+
   return (
     <S.Container>
       <S.Section>
@@ -19,29 +136,49 @@ const JobRegisterIdPage = () => {
           <S.Title>공고 등록 편집</S.Title>
           <Image src={CloseIcon} alt="CloseIcon" />
         </S.TitleWrap>
-        <S.Wraps>
-          <S.InputWrap>
-            <S.InputWrapLabel>시급*</S.InputWrapLabel>
-            <S.TextWon>원</S.TextWon>
-            <S.Input type="number" placeholder="입력" />
-          </S.InputWrap>
-          <S.InputWrap>
-            <S.InputWrapLabel>시작 일시*</S.InputWrapLabel>
-            <S.Input type="datetime-local" />
-          </S.InputWrap>
-          <S.InputWrap>
-            <S.InputWrapLabel>업무 시간*</S.InputWrapLabel>
-            <S.TextWon>시간</S.TextWon>
-            <S.Input type="number" placeholder="입력" />
-          </S.InputWrap>
-        </S.Wraps>
-        <S.Wraps>
-          <S.InputWrapExplain>
-            <S.InputWrapLabel>공고 설명</S.InputWrapLabel>
-            <S.TextBox placeholder="입력" />
-          </S.InputWrapExplain>
-        </S.Wraps>
-        <S.Button onClick={handleOpen}>편집하기</S.Button>
+        <S.Form onSubmit={handleSubmit}>
+          <S.Wraps>
+            <S.InputWrap>
+              <S.InputWrapLabel>시급*</S.InputWrapLabel>
+              <S.TextWon>원</S.TextWon>
+              <S.Input
+                type="number"
+                placeholder="입력"
+                value={pay}
+                onChange={payInput}
+              />
+            </S.InputWrap>
+            <S.InputWrap>
+              <S.InputWrapLabel>시작 일시*</S.InputWrapLabel>
+              <S.Input
+                type="datetime-local"
+                value={time}
+                onChange={handleTime}
+              />
+            </S.InputWrap>
+            <S.InputWrap>
+              <S.InputWrapLabel>업무 시간*</S.InputWrapLabel>
+              <S.TextWon>시간</S.TextWon>
+              <S.Input
+                type="number"
+                placeholder="입력"
+                value={workTime}
+                onChange={handleWorkTime}
+              />
+            </S.InputWrap>
+          </S.Wraps>
+          <S.Wraps>
+            <S.InputWrapExplain>
+              <S.InputWrapLabel>공고 설명</S.InputWrapLabel>
+              <S.TextBox
+                placeholder="입력"
+                value={jobExplain}
+                onChange={textAreaInput}
+              />
+            </S.InputWrapExplain>
+          </S.Wraps>
+          <S.Button type="submit">편집하기</S.Button>
+        </S.Form>
         <Modal open={open} onClose={handleClose}>
           <S.ModalBox>
             <S.ModalText>편집이 완료되었습니다.</S.ModalText>

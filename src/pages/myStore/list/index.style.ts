5023cd10f16f19e50f9a6fb5553fd12237c29623
summerlane <punkwagon@gmail.com>
2026-01-06 01:41:45 +0900

    chore: 스타일 추가

--- src/pages/myStore/list/index.style.ts
+++ src/pages/myStore/list/index.style.ts
@@ -597,3 +597,71 @@ export const ArrowIconDisable = styled(ArrowIconComponent)`
     }
   }
 `;
+
+export const Container1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  align-items: center;
+  min-height: 100vh;
+  padding: 40px 12px;
+  margin: 0 auto;
+
+  @media ${media.tablet} {
+    padding: 60px 32px;
+  }
+
+  @media ${media.desktop} {
+    max-width: 965px;
+    padding: 80px 0;
+  }
+`;
+
+export const Button = styled.button`
+  width: 110px;
+  height: 37px;
+  padding: 10px 20px;
+  font-size: 14px;
+  font-weight: 700;
+  color: ${colors.white};
+  letter-spacing: -1.5px;
+  background-color: ${colors.red[40]};
+  border-radius: 6px;
+
+  @media ${media.tablet} {
+    min-width: 346px;
+    height: 47px;
+    font-size: 16px;
+  }
+`;
+
+export const Title1 = styled.h1`
+  width: 100%;
+  font-size: 20px;
+  font-weight: 700;
+  text-align: left;
+
+  @media ${media.tablet} {
+    font-size: 28px;
+  }
+`;
+
+export const Card1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  align-items: center;
+  width: 100%;
+  padding: 60px 24px;
+  border: 1px solid ${colors.gray[20]};
+  border-radius: 12px;
+`;
+
+export const CardText = styled.p`
+  font-size: 14px;
+  color: ${colors.black};
+
+  @media ${media.tablet} {
+    font-size: 16px;
+  }
+`;

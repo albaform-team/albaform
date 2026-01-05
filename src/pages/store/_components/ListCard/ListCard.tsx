b352b95ff658d473f550c0ab103ad47a1901409c
seoyoung <nct830@naver.com>
2026-01-06 00:25:38 +0900

    refactor : PR 리뷰 반영 -> utils/date.ts formatDateTimeRange으로 카드리스트 날짜 시간 구현

--- src/pages/store/_components/ListCard/ListCard.tsx
+++ src/pages/store/_components/ListCard/ListCard.tsx
@@ -1,10 +1,8 @@
 import Image from 'next/image';
 
-import ArrowIcon from '@/assets/svg/arrow-up.svg';
 // import ClockIcon from '@/assets/svg/clock.svg';
-import ClockIconComponent from '@/assets/svg/clockicon';
-import LocationIcon from '@/assets/svg/location.svg';
 import * as S from '@/pages/store/_components/ListCard/ListCard.styles';
+import { formatDateTimeRange } from '@/utils/date';
 
 import { NoticeItem } from './types/mockNotices';
 
@@ -22,26 +20,6 @@ const ListCard = ({ notice }: ListCardProps) => {
       item: { name, address1, imageUrl, originalHourlyPay },
     },
   } = notice;
-
-  const formatDateWithWorkTime = (startsAt: string, workhour: number) => {
-    const date = new Date(startsAt);
-
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(date.getUTCDate()).padStart(2, '0');
-
-    const startHour = date.getUTCHours();
-    const startMinute = date.getUTCMinutes();
-
-    const endHour = startHour + workhour;
-    const endMinute = startMinute;
-
-    const pad = (n: number) => String(n).padStart(2, '0');
-
-    return `${year}.${month}.${day} ${pad(startHour)}:${pad(
-      startMinute
-    )} ~ ${pad(endHour)}:${pad(endMinute)} (${workhour}시간)`;
-  };
 
   const increaseRatePercent =
     Math.floor((hourlyPay - originalHourlyPay) / originalHourlyPay) * 100;
@@ -62,24 +40,14 @@ const ListCard = ({ notice }: ListCardProps) => {
             <S.JobTitle isClosed={closed}>{name}</S.JobTitle>
             <S.JobMetaSection>
               <S.ClockIcon isClosed={closed} />
-              {/* <S.JobMetaIcon
-                src={ClockIcon}
-                alt="근무 시간"
-                width={16}
-                height={16}
-              /> */}
               <S.JobMetaInfo isClosed={closed}>
-                {formatDateWithWorkTime(startsAt, workhour)}{' '}
+                {formatDateTimeRange(startsAt, {
+                  durationHours: workhour,
+                })}{' '}
               </S.JobMetaInfo>
             </S.JobMetaSection>
             <S.JobMetaSection>
               <S.NavIcon isClosed={closed} />
-              {/* <S.JobMetaIcon
-                src={LocationIcon}
-                alt="근무 장소"
-                width={16}
-                height={16}
-              /> */}
               <S.JobMetaInfo isClosed={closed}>{address1}</S.JobMetaInfo>
             </S.JobMetaSection>
           </S.JobSummary>
@@ -91,12 +59,6 @@ const ListCard = ({ notice }: ListCardProps) => {
                   기존 시급보다 {increaseRatePercent}%️️
                 </S.PayIncreaseBadge>
                 <S.ArrowIcon />
-                {/* <S.JobMetaIcon
-                  src={ArrowIcon}
-                  alt="상승"
-                  width={16}
-                  height={16}
-                /> */}
               </S.PayIncreaseBadgeSection>
             )}
           </S.PaySection>

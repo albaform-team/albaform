97b22d2f0608a76cea8b90219905af39e9132b23
spde3289 <spde3289@naver.com>
2026-01-05 22:51:07 +0900

    feat: 인증 상태에 따른 헤더 변경

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,25 +5,29 @@ import { useState } from 'react';
 
 import LogoImage from '@/assets/svg/logo.svg';
 import NotificationImage from '@/assets/svg/notification.svg';
-import SearchImage from '@/assets/svg/search.svg';
 import {
   AUTH_ROUTES,
   MY_STORE_ROUTES,
   PROFILE_ROUTES,
+  STORE_ROUTES,
 } from '@/constants/routes';
+import useAuthStore from '@/stores/useAuthStore';
 
 import NotificationModal from './_components/notificationModal/NotificationModal';
 import SearchForm from './_components/Search/SearchForm';
 import * as S from './Header.styles';
 
-type UserType = 'unlogin' | 'owner' | 'user';
+// type UserType = 'unlogin' | 'owner' | 'user';
 
-type HeaderType = {
-  user: UserType;
-};
+// type HeaderType = {
+//   user: UserType;
+// };
 
-const Header = ({ user = 'unlogin' }: HeaderType) => {
+const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const user = useAuthStore(s => s.user);
+  const clearAuth = useAuthStore(s => s.clearAuth);
+
   const router = useRouter();
   const { q } = router.query;
 
@@ -35,7 +39,7 @@ const Header = ({ user = 'unlogin' }: HeaderType) => {
       <S.HeaderContainer>
         <S.HeaderContent>
           <S.HeaderLeft>
-            <S.LogoLink href={'/store'}>
+            <S.LogoLink href={STORE_ROUTES.ROOT}>
               <S.LogoImage
                 src={LogoImage}
                 alt="메인로고"
@@ -44,20 +48,20 @@ const Header = ({ user = 'unlogin' }: HeaderType) => {
               />
             </S.LogoLink>
           </S.HeaderLeft>
-
           <SearchForm initialValue={q} />
-
           <S.HeaderRight>
-            {user === 'unlogin' && (
+            {user?.type === undefined && (
               <>
                 <S.Button href={AUTH_ROUTES.LOGIN}>로그인</S.Button>
                 <S.Button href={AUTH_ROUTES.SIGN_UP}>회원가입</S.Button>
               </>
             )}
-            {user === 'owner' && (
+            {user?.type === 'employer' && (
               <>
                 <S.Button href={MY_STORE_ROUTES.ROOT}>내 가게</S.Button>
-                <S.Button href={AUTH_ROUTES.SIGN_UP}>로그아웃</S.Button>
+                <S.Button onClick={clearAuth} href={AUTH_ROUTES.SIGN_UP}>
+                  로그아웃
+                </S.Button>
                 <S.NotificationButton onClick={openNotification}>
                   <Image
                     src={NotificationImage}
@@ -68,12 +72,14 @@ const Header = ({ user = 'unlogin' }: HeaderType) => {
                 </S.NotificationButton>
               </>
             )}
-            {user === 'user' && (
+            {user?.type === 'employee' && (
               <>
-                <S.Button href={PROFILE_ROUTES.ROOT('testUserId')}>
+                <S.Button href={PROFILE_ROUTES.ROOT(user.id)}>
                   내 프로필
                 </S.Button>
-                <S.Button href={AUTH_ROUTES.SIGN_UP}>로그아웃</S.Button>
+                <S.Button onClick={clearAuth} href={AUTH_ROUTES.SIGN_UP}>
+                  로그아웃
+                </S.Button>
                 <S.NotificationButton onClick={openNotification}>
                   <Image
                     src={NotificationImage}

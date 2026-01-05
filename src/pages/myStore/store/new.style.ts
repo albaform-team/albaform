edcfd287baf9ee0aad0e76b41942889c2bbd022b
summerlane <punkwagon@gmail.com>
2026-01-05 18:12:53 +0900

    chore: 파일 인풋 스타일링

--- src/pages/myStore/store/new.style.ts
+++ src/pages/myStore/store/new.style.ts
@@ -169,6 +169,15 @@ export const StoreImgBox = styled.div`
   background-color: ${colors.gray[10]};
   border: 1px solid ${colors.gray[30]};
   border-radius: 6px;
+  position: relative;
+`;
+
+export const FileInput = styled.input`
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  top: 30px;
+  opacity: 0;
 `;
 
 export const TextBox = styled.textarea`

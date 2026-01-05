fea8ab208ca3fe28ed42512098b2948a29652a33
summerlane <punkwagon@gmail.com>
2026-01-05 18:11:20 +0900

    feat: 이미지 컴포넌트 추가

---
+++ src/pages/myStore/store/_components/storeimg2.style.ts
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+import styled from '@emotion/styled';
+
+export const StoreImgStyle = styled(Image)`
+  object-fit: cover;
+  object-position: 50% 50%;
+  border-radius: 6px;
+`;

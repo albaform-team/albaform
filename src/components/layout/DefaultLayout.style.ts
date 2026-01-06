edeea6ce21d89b82da460081b02655323a8a0cf0
spde3289 <spde3289@naver.com>
2026-01-06 16:31:40 +0900

    feat: 공고 상세 정보 api 연결

--- src/components/layout/DefaultLayout.style.ts
+++ src/components/layout/DefaultLayout.style.ts
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
 
+import { media } from '@/styles/media';
+
 export const Main = styled.main`
   min-height: calc(100vh - 202px);
+
+  @media ${media.tablet} {
+    min-height: calc(100vh - 170px);
+  }
 `;

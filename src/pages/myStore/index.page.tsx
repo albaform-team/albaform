20a9a394704dcd0d80d8efd3c0f56f93349f1b32
spde3289 <spde3289@naver.com>
2026-01-07 14:29:42 +0900

    fix: 무한 스크롤 버그 수정

--- src/pages/myStore/index.page.tsx
+++ src/pages/myStore/index.page.tsx
@@ -2,20 +2,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+import { isAxiosError } from 'axios';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import withAuthentication from '@/components/hoc/withAuthentication';
-import { NOTICES_API, USERS_API } from '@/constants/api';
+import { NOTICES_API } from '@/constants/api';
 import { MY_STORE_ROUTES } from '@/constants/routes';
 import useInfinitePagination from '@/hooks/useInfinitePagination';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import { services } from '@/lib/services/servicesClient';
 import useAuthStore from '@/stores/useAuthStore';
+import { formatDateTimeRange } from '@/utils/date';
 
 import * as S from './index.style';
 
 const StoreListIdPage = () => {
+  const user = useAuthStore(s => s.user);
   // 가게 이름
+
   const [storeName, setStoreName] = useState('');
 
   // 분류
@@ -30,8 +34,8 @@ const StoreListIdPage = () => {
   // 가게 설명
   const [textExplain, setTextExplain] = useState('');
 
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const jobId = router.query.id;
 
   type Job = {
     item: {
@@ -44,124 +48,81 @@ const StoreListIdPage = () => {
   };
   const [showJob, setShowJob] = useState<Job[]>([]);
   const [shopId, setShopId] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
 
-  const { meta, limit, reset, updateFromResponse } = useInfinitePagination(10);
-
-  const changeTime = () => {
-    if (showJob.length === 0) return '';
-
-    const date = new Date(showJob[0].item.startsAt);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    const hour = String(date.getHours()).padStart(2, '0');
-    const minute = String(date.getMinutes()).padStart(2, '0');
-
-    return `${year}-${month}-${day} ${hour}:${minute}`;
-  };
+  const { meta, limit, reset, updateFromResponse } = useInfinitePagination(6);
 
   const handleEdit = () => {
     router.push(MY_STORE_ROUTES.STORE.EDIT);
   };
 
   useEffect(() => {
-    const importData = async () => {
-      const { user } = useAuthStore.getState();
-      const getUserId = user?.id;
-      if (!getUserId) return;
-      const getShop = await services.get(USERS_API.ME(getUserId));
+    if (!user?.id) return;
 
-      const shopData = getShop?.data?.item?.shop?.item;
-      if (!shopData) return;
+    const shopData = user.shop?.item;
+    if (!shopData) return;
 
-      const shopId = getShop.data.item.shop.item.id;
+    const shopId = user.shop?.item.id;
 
-      setShopId(shopId);
-      setStoreName(shopData.name);
-      setCategorySelected(shopData.category);
-      setAddressSelected(shopData.address1);
-      setImgFile(shopData.imageUrl);
-      setTextExplain(shopData.description);
-
-      const getNotice = await services.get(NOTICES_API.SHOP_LIST(shopId));
-
-      const getJob = getNotice.data.items;
-      setShowJob(getJob);
-    };
-
-    importData();
-  }, [jobId]);
+    setShopId(shopId as string);
+    setStoreName(shopData.name);
+    setCategorySelected(shopData.category);
+    setAddressSelected(shopData.address1);
+    setImgFile(shopData.imageUrl);
+    setTextExplain(shopData.description);
+  }, [user?.id, user?.shop?.item]);
 
   // 무한 스크롤
-  const fetchNext = useCallback(async () => {
-    console.log('fetchNext called', {
-      offset: meta.offset,
-      hasNext: meta.hasNext,
-      isLoading,
-    });
-    if (!shopId) return;
-    if (isLoading || !meta.hasNext) return;
+  const fetchNext = useCallback(
+    async (mode: 'replace' | 'append', offset: number) => {
+      if (!user?.shop?.item.id) return;
 
-    setIsLoading(true);
-    try {
-      const res = await services.get(NOTICES_API.SHOP_LIST(shopId), {
-        params: { offset: meta.offset, limit },
-      });
+      setIsLoading(true);
+      try {
+        const res = await services.get(
+          NOTICES_API.SHOP_LIST(user?.shop?.item.id),
+          {
+            params: { offset, limit },
+          }
+        );
 
-      const data = res.data;
+        const data = res.data;
 
-      setShowJob(prev => [...prev, ...data.items]);
+        setShowJob(prev =>
+          mode === 'replace' ? data.items : [...prev, ...data.items]
+        );
 
-      updateFromResponse({
-        offset: data.offset,
-        limit: data.limit,
-        count: data.count,
-        hasNext: data.hasNext,
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  }, [shopId, isLoading, meta.hasNext, meta.offset, limit, updateFromResponse]);
+        updateFromResponse(data);
+      } catch (e) {
+        if (isAxiosError(e)) {
+          alert(e.response?.data.message ?? '잠시후에 다시 시도해 주세요');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [limit, updateFromResponse, user?.shop?.item.id]
+  );
 
   useEffect(() => {
     if (!shopId) return;
 
     reset();
     setShowJob([]);
-    (async () => {
-      setIsLoading(true);
-      try {
-        const res = await services.get(NOTICES_API.SHOP_LIST(shopId), {
-          params: { offset: 0, limit },
-        });
-
-        const data = res.data;
-
-        setShowJob(data.items);
-
-        updateFromResponse({
-          offset: data.offset,
-          limit: data.limit,
-          count: data.count,
-          hasNext: data.hasNext,
-        });
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, [shopId, limit, reset, updateFromResponse]);
+    fetchNext('replace', 0);
+  }, [fetchNext, reset, shopId]);
 
   const enabled = useMemo(
-    () => !!shopId && meta.hasNext && !isLoading,
-    [shopId, meta.hasNext, isLoading]
+    () => !isLoading && meta.hasNext,
+    [isLoading, meta.hasNext]
   );
 
   const targetRef = useIntersectionObserver<HTMLDivElement>({
-    onIntersect: fetchNext,
+    onIntersect: () => {
+      if (!meta.hasNext) return;
+      if (isLoading) return;
+      fetchNext('append', meta.offset);
+    },
     threshold: 0,
-    rootMargin: '200px 0px',
     enabled,
   });
 
@@ -288,14 +249,15 @@ const StoreListIdPage = () => {
                 <S.CardListTextWrap>
                   <S.CardListTitleWrap>
                     <S.CardListTitle $closed={i.item.closed}>
-                      {storeName}
+                      {i.item.id}
                     </S.CardListTitle>
                   </S.CardListTitleWrap>
                   <S.CardListNavWrap01>
                     <S.ClockIcon $closed={i.item.closed} />
                     <S.CardListNavText $closed={i.item.closed}>
-                      {changeTime()} ({i.item.workhour}
-                      시간)
+                      {formatDateTimeRange(i.item.startsAt.toString(), {
+                        durationHours: i.item.workhour,
+                      })}
                     </S.CardListNavText>
                   </S.CardListNavWrap01>
                   <S.CardListNavWrap>
@@ -318,9 +280,7 @@ const StoreListIdPage = () => {
                 </S.CardPriceTextWrap>
               </S.CardList>
             ))}
-
-            <div ref={targetRef} style={{ height: 1 }}></div>
-            {isLoading && <div>Loading...</div>}
+            <div ref={targetRef}></div>
             {!meta.hasNext && <div></div>}
           </S.CardListWrap>
         </S.Section02>
